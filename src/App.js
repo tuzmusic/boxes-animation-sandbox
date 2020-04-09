36eea96de007e5dade9101375efbf32b7b32f473
Jonathan Tuzman <tuzmusic@gmail.com>
2020-04-09 13:50:05 -0400

    initial before SO question

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
-import "./styles.css";
+import "./appearance.css";
+import "./animation.css";
 
 export default function App() {
   const clickBox = i => setters[i](!states[i]);
 
+  const [clicked0, setClicked0] = useState(false);
   const [clicked1, setClicked1] = useState(false);
   const [clicked2, setClicked2] = useState(false);
-  const [clicked3, setClicked3] = useState(false);
 
-  const states = [clicked1, clicked2, clicked3];
-  const setters = [setClicked1, setClicked2, setClicked3];
+  const states = [clicked0, clicked1, clicked2];
+  const setters = [setClicked0, setClicked1, setClicked2];
 
   function Box({ index }) {
     let className = "box";
@@ -24,9 +25,10 @@ export default function App() {
 
   const Boxes = [0, 1, 2].map(i => <Box index={i} key={i} />);
   const Buttons = [0, 1, 2].map(i => (
-    <button onClick={() => clickBox(i)}>{i}</button>
+    <button onClick={() => clickBox(i)} key={i}>
+      {i}
+    </button>
   ));
-  console.log(states);
 
   return (
     <div className="app">
